0119e56dc3241eacab93ab16a4c199a48ec45f0e
MichalKafka <kafkins@gmail.com>
2023-06-17 10:40:55 +0200

    rel 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MS Kafkami' });
+  res.render('index', { title: 'tohle je uplne novy test CI/CD' });
 });
 
 module.exports = router;
